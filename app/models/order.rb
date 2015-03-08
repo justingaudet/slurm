4b1f951d4dc63f8262e3efef5fe5cdc44d4b8420
justingaudet <jgaudet91@gmail.com>
2015-03-07 23:59:40 -0800

    minor visual changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
 class Order < ActiveRecord::Base
   validates :name, :presence => true
-  validates :amount, :length => { :in => 1..4, :message => " must be between 1,9999"}
+  validates :amount, :presence => true
 
-  def cost=(amount)
-    write_attribute(:cost, amount*10+10)
+  def cost=(num)
+    write_attribute(:cost, (:amount.to_i*10+10).to_i)
   end
 
 
