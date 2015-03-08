9f37f65fcba437484c7ed21848a1034637c804be
justingaudet <jgaudet91@gmail.com>
2015-03-07 23:21:47 -0800

    added validation in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :amount, :length => { :in => 1..4, :message => " must be between 1,9999"}
+
+  def cost=(amount)
+    write_attribute(:cost, amount*10+10)
+  end
+
+
 end
