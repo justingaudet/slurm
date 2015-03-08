98747ca236a4dce619f684315142111581a5b9c2
justingaudet <jgaudet91@gmail.com>
2015-03-08 13:10:21 -0700

    added basic pusher functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
+require 'pusher'
+
+Pusher.app_id = '110230'
+Pusher.key = '63a00516bd7a894567e2'
+Pusher.secret = 'fb24967344b1999a78e4'
+
 class WelcomeController < ApplicationController
   def index
+    Pusher['test_channel'].trigger('my_event', {message: 'hello world'})
   end
 end
